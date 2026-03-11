35b1d8b0087b79e8fe2ea26bf42adf7f9c4e37be
Bala murugun Mohan <balamurugun530@gmail.com>
2026-03-11 12:47:17 +0530

    Fix image loading with assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/invicta-2k26' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/invicta-2k26' : '',
   images: {
     unoptimized: true,
     qualities: [100, 75],
